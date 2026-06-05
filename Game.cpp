db8ac97a922d938328314f3fab34da385de1e732
MoPeoplesDevs <MoPeoplesDevs@outlook.com>
2026-06-05 05:23:44 -0600

    created if the player loses

--- Game.cpp
+++ Game.cpp
@@ -88,18 +88,28 @@ void Game::Render() const
 
 void Game::CheckCollision()
 {
-	// TODO #4 - Update collision to check all bricks
-	if (brick.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+	for (int i = 0; i < brick.size(); i++) 
 	{
-		brick.color = ConsoleColor(brick.color - 1);
-		ball.y_velocity *= -1;
+		// TODO #4 - Update collision to check all bricks
+		if (brick[i].Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+		{
+			brick[i].color = ConsoleColor(brick[i].color - 1);
+			ball.y_velocity *= -1;
 
-		// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
-
+			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
+			if (brick[i].color == ConsoleColor::Black)
+			{
+				brick.erase(brick.begin() + i);
+			}
+		}
 	}
 
 	// TODO #6 - If no bricks remain, pause ball and display (render) victory text with R to reset
-
+	if (brick.size() == 0)
+	{
+		ball.moving = false;
+		Console::WordWrap(25, 15, 30, "Victory, Press R to play agian!");
+	}
 
 	if (paddle.Contains(ball.x_position + ball.x_velocity, ball.y_velocity + ball.y_position))
 	{
@@ -107,4 +117,9 @@ void Game::CheckCollision()
 	}
 
 	// TODO #7 - If ball touches bottom of window, pause ball and display (render) defeat text with R to reset
+
+	if (ball.y_position >= WINDOW_HEIGHT)
+	{
+		ball.moving = false
+	}
 }
